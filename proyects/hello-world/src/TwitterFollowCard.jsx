ba98868eaa0416a181f0f6b41c61840f0f4a3dcd
DanielR9905 <danielramirezduque1@gmail.com>
2023-08-09 07:49:24 -0500

    First component tw follow Card

---
+++ proyects/hello-world/src/TwitterFollowCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function TwitterFollowCard({ userName, name, isFollowing }) {
+
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          src={`https://unavatar.io/${userName}`}
+          alt="Avatar del perfil"
+          className="tw-followCard-avatar"
+        />
+        <div className="tw-followCard-info">
+          <strong>{name}</strong>
+          <span>@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </article>
+  );
+}
