8db1be268c79a03ab1a22df6d6768ab855760b7f
DanielR9905 <danielramirezduque1@gmail.com>
2023-08-17 13:47:01 -0500

    Progress 2 tic tac toe

--- proyects/02-tic-tac-toe/src/App.jsx
+++ proyects/02-tic-tac-toe/src/App.jsx
@@ -5,23 +5,48 @@ const TURNS = {
   O: "o",
 };
 
-const Squared = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Squared = ({ children, isSelected, updateBoard, index }) => {
+  
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () =>{
+    updateBoard(index)
+  }
+  
+  return <div onClick={handleClick} className={className}>{children}</div>;
 };
+
+
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  console.log(board);
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn // x u o 
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Squared key={index} index={index}>
-
+            <Squared key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
             </Squared>
           );
         })}
+      </section>
+
+      <section className="turn">
+        <Squared isSelected={turn === TURNS.X}>{TURNS.X}</Squared>
+        <Squared isSelected={turn === TURNS.O}>{TURNS.O}</Squared>
       </section>
     </main>
   );
