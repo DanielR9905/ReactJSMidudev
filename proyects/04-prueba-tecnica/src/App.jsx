aff2496a0f9192343bad595b98c9f2da26bd95ab
DanielR9905 <danielramirezduque1@gmail.com>
2023-08-24 16:44:28 -0500

    Progress fetching data 04-prueba-tecnica 2

--- proyects/04-prueba-tecnica/src/App.jsx
+++ proyects/04-prueba-tecnica/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`;
 
 export function App() {
 const [fact, setFact] = useState()
@@ -12,10 +12,16 @@ useEffect(()=>{
         .then(data => {
             const {fact} = data
             setFact(fact)
-            
-            const firstWord = fact.split(' ').slice(0, 3).join(' ')
-            console.log(firstWord);
+            //conseguimos las 3 primeras palabras
+            const threeFirstWords = fact.split(' ', 3)
+            console.log(threeFirstWords);
         })
+
+    fetch(CAT_ENDPOINT_IMAGE_URL)
+    .then(res => res.json()
+    .then(response => {
+      console.log(response);
+    }))
 },[])
   return (
     <main>
