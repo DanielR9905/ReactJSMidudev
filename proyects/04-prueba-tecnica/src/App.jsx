24b627b30dc7021e054b1a3400d02842afc18bbf
DanielR9905 <danielramirezduque1@gmail.com>
2023-08-25 09:54:27 -0500

    Upgrade app setting error in service cat

--- proyects/04-prueba-tecnica/src/App.jsx
+++ proyects/04-prueba-tecnica/src/App.jsx
@@ -1,32 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`;
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`;
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 export function App() {
-const [fact, setFact] = useState()
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [facterror, setFactError] = useState()
 
-useEffect(()=>{
+  //Efecto para recuperar la cita al cargar la pàgina
+  useEffect(() => {
+    //Este fetch devuelve una promesa
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-        .then(res => res.json())
-        .then(data => {
-            const {fact} = data
-            setFact(fact)
-            //conseguimos las 3 primeras palabras
-            const threeFirstWords = fact.split(' ', 3)
-            console.log(threeFirstWords);
-        })
+      //devuelve una promesa
+      .then((res) => {
+        //TODO: Handle error if !res.ok
+        return res.json()
+      })
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
 
-    fetch(CAT_ENDPOINT_IMAGE_URL)
-    .then(res => res.json()
-    .then(response => {
-      console.log(response);
-    }))
-},[])
+  //Efecto para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if(!fact) return
+    //conseguimos las 3 primeras palabras
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+    console.log(threeFirstWords);
+
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        // console.log(response);
+        const { url } = response;
+        setImageUrl(url);
+      });
+  },[fact]);
+
   return (
     <main>
-        <h1>App de gatitos</h1>
-        {fact && <p> {fact} </p>}
+      <h1>App de gatitos</h1>
+      {fact && <p> {fact} </p>}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={`Image extrated using the first three words for ${fact}`}
+        />
+      )}
     </main>
-  )
+  );
 }
