1883c201cd83038cbf2b40758a82991053255a6c
DanielR9905 <danielramirezduque1@gmail.com>
2023-08-25 14:37:46 -0500

    Finish 04-prueba-tecnica

--- proyects/04-prueba-tecnica/src/App.jsx
+++ proyects/04-prueba-tecnica/src/App.jsx
@@ -1,54 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import './App.css'
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
-const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`;
-const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-export function App() {
-  const [fact, setFact] = useState();
-  const [imageUrl, setImageUrl] = useState();
-
-  //Efecto para recuperar la cita al cargar la pàgina
-  useEffect(() => {
-    //Este fetch devuelve una promesa
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      //devuelve una promesa
-      .then((res) => res.json())
-      .then((data) => {
-        const { fact } = data;
-        setFact(fact);
-      });
-  }, []);
-
-  //Efecto para recuperar la imagen cada vez que tenemos una cita nueva
-  useEffect(() => {
-    if(!fact) return
-    //conseguimos las 3 primeras palabras
-    const threeFirstWords = fact.split(" ", 3).join(" ");
-    console.log(threeFirstWords);
-
-    fetch(
-      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
-    )
-      .then((res) => res.json())
-      .then((response) => {
-        // console.log(response);
-        const { url } = response;
-        setImageUrl(url);
-      });
-  },[fact]);
+  const handleClick = async () => {
+    refreshFact()
+  }
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      {fact && <p> {fact} </p>}
-      {imageUrl && (
-        <img
-          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
-          alt={`Image extrated using the first three words for ${fact}`}
-        />
-      )}
+
+      <button onClick={handleClick}>Get new fact</button>
+
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first rhee words for ${fact}`} />}
     </main>
-  );
+  )
 }
