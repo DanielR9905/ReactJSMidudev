2c8ab52ff2fd42dd693b06ef963c6cf639af3e1f
DanielR9905 <danielramirezduque1@gmail.com>
2023-09-08 10:02:21 -0500

    First commit poryect 06-shopping card

---
+++ proyects/06-shoping-cart/src/App.jsx
@@ -0,0 +1,8 @@
+import { products } from "./mocks/products.json";
+import { Products } from "./components/Products.jsx";
+
+function App() {
+  return <Products products={products} />;
+}
+
+export default App;
