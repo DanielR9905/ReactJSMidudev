7f449bb00215cc72f771fa83c2d61f0528118675
DanielR9905 <danielramirezduque1@gmail.com>
2023-10-05 16:57:11 -0500

    Progress 10-prueba-tecnica-empresa-55k

---
+++ proyects/10-prueba-tecnica-empresa-55k/src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { type User } from './types.d'
+import { UsersList } from './components/UsersList'
+
+function App() {
+  const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
+
+  const toggleColors = () =>{
+    setShowColors(!showColors)
+  }
+
+  const toggleSortByCountry = () =>{
+    setSortByCountry(prevState => !prevState)
+  }
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+    .then(async res => await res.json())
+    .then(res => {
+      setUsers(res.results)
+    })
+    .catch(err => {
+      console.error(err);
+      
+    })
+  },[])
+  
+  const sortedUsers = sortByCountry ? [...users].sort((a,b) => {
+    return a.location.country.localeCompare(b.location.country)
+  }) : users
+
+  return (
+    <div className='App'>
+        <h1>Prueba Tecnica</h1>
+        <header>
+          <button onClick={toggleColors}>Colorear filas</button>
+          <button onClick={toggleSortByCountry}>{sortByCountry ? 'No ordenar por pais' : 'Ordenar por pais'}</button>
+        </header>
+        <main>
+          <UsersList showColors={showColors} users={sortedUsers}/>
+        </main>
+    </div>
+  )
+}
+
+export default App
---
+++ proyects/10-prueba-tecnica-empresa-55k/src/components/UsersList.tsx
@@ -0,0 +1,50 @@
+import { type User } from "../types.d"
+
+interface Props {
+    showColors: boolean,
+    users: User[]
+}
+export function UsersList({ showColors, users } : Props) {
+  return (
+    <table width='100%'>
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>País</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                users.map((user, index) => {
+                    const backgroundColor = index % 2 === 0 ? '#333' : '#555'
+                    const color = showColors ? backgroundColor : 'transparent'
+                    return (
+                        <tr key={index} style={{backgroundColor: color}}>
+                            <td>
+                                <img src={user.picture.thumbnail}/>
+                            </td>
+                            <td>
+                                {user.name.first}
+                            </td>
+                            <td>
+                                {user.name.last}
+                            </td>
+                            <td>
+                                {user.location.country}
+                            </td>
+                            <td>
+                                <button>
+                                    Borrar
+                                </button>
+                            </td>
+                        </tr>
+                    )
+                } )
+            }
+        </tbody>
+    </table>
+  )
+}
