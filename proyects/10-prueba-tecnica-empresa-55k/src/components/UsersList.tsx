84cf02fedf88517d44249c29aa58ec9bd67dfdf4
DanielR9905 <danielramirezduque1@gmail.com>
2023-10-06 13:22:11 -0500

    Finish 10-prueba-tecnica-empresa-55k

--- proyects/10-prueba-tecnica-empresa-55k/src/components/UsersList.tsx
+++ proyects/10-prueba-tecnica-empresa-55k/src/components/UsersList.tsx
@@ -1,50 +1,52 @@
-import { type User } from "../types.d"
+import { SortBy, type User } from '../types.d'
 
 interface Props {
-    showColors: boolean,
-    users: User[]
+  changeSorting: (sort: SortBy) => void
+  deleteUser: (email: string) => void
+  showColors: boolean
+  users: User[]
 }
-export function UsersList({ showColors, users } : Props) {
+
+export function UsersList ({ changeSorting, deleteUser, showColors, users }: Props) {
   return (
     <table width='100%'>
-        <thead>
-            <tr>
-                <th>Foto</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>País</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                users.map((user, index) => {
-                    const backgroundColor = index % 2 === 0 ? '#333' : '#555'
-                    const color = showColors ? backgroundColor : 'transparent'
-                    return (
-                        <tr key={index} style={{backgroundColor: color}}>
-                            <td>
-                                <img src={user.picture.thumbnail}/>
-                            </td>
-                            <td>
-                                {user.name.first}
-                            </td>
-                            <td>
-                                {user.name.last}
-                            </td>
-                            <td>
-                                {user.location.country}
-                            </td>
-                            <td>
-                                <button>
-                                    Borrar
-                                </button>
-                            </td>
-                        </tr>
-                    )
-                } )
-            }
-        </tbody>
+      <thead>
+        <tr>
+          <th>Foto</th>
+          <th className='pointer' onClick={() => { changeSorting(SortBy.NAME) }}>Nombre</th>
+          <th className='pointer' onClick={() => { changeSorting(SortBy.LAST) }}>Apellido</th>
+          <th className='pointer' onClick={() => { changeSorting(SortBy.COUNTRY) }}>País</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+
+      <tbody className={showColors ? 'table--showColors' : ''}>
+        {
+          users.map((user) => {
+            return (
+              <tr key={user.email}>
+                <td>
+                  <img src={user.picture.thumbnail} />
+                </td>
+                <td>
+                  {user.name.first}
+                </td>
+                <td>
+                {user.name.last}
+                </td>
+                <td>
+                  {user.location.country}
+                </td>
+                <td>
+                  <button onClick={() => {
+                    deleteUser(user.email)
+                  }}>Borrar</button>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
     </table>
   )
 }
