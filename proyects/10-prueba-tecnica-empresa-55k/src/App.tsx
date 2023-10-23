8cda56b397efa8d954d2894db6c01b0d3da71ba3
DanielR9905 <danielramirezduque1@gmail.com>
2023-10-23 13:57:41 -0500

    Progress #10 prueba tecnica 55k - add paginate

--- proyects/10-prueba-tecnica-empresa-55k/src/App.tsx
+++ proyects/10-prueba-tecnica-empresa-55k/src/App.tsx
@@ -1,124 +1,152 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import './App.css'
-import { UsersList } from './components/UsersList'
-import { SortBy, type User } from './types.d'
+import { useEffect, useMemo, useRef, useState } from "react";
+import "./App.css";
+import { UsersList } from "./components/UsersList";
+import { SortBy, type User } from "./types.d";
 
-function App () {
-  const [users, setUsers] = useState<User[]>([])
-  const [showColors, setShowColors] = useState(false)
-  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
-  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+const fetchUsers = async(page: number) => {
+  return await fetch(
+    `https://randomuser.me/api?results=10&seed=midudev&page=${page}`
+  )
+    .then(async (res) => {
+      if (!res.ok) throw new Error("Error en la petición");
+      return await res.json();
+    })
+    .then(res => res.results)
+}
 
-  const originalUsers = useRef<User[]>([])
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [showColors, setShowColors] = useState(false);
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const originalUsers = useRef<User[]>([]);
   // useRef -> para guardar un valor
   // que queremos que se comparta entre renderizados
   // pero que al cambiar, no vuelva a renderizar el componente
 
   const toggleColors = () => {
-    setShowColors(!showColors)
-  }
+    setShowColors(!showColors);
+  };
 
   const toggleSortByCountry = () => {
-    const newSortingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
-    setSorting(newSortingValue)
-  }
+    const newSortingValue =
+      sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE;
+    setSorting(newSortingValue);
+  };
 
   const handleReset = () => {
-    setUsers(originalUsers.current)
-  }
+    setUsers(originalUsers.current);
+  };
 
   const handleDelete = (email: string) => {
-    const filteredUsers = users.filter((user) => user.email !== email)
-    setUsers(filteredUsers)
-  }
+    const filteredUsers = users.filter((user) => user.email !== email);
+    setUsers(filteredUsers);
+  };
 
   const handleChangeSort = (sort: SortBy) => {
-    setSorting(sort)
-  }
+    setSorting(sort);
+  };
 
   useEffect(() => {
-    fetch('https://randomuser.me/api?results=100')
-      .then(async res => await res.json())
-      .then(res => {
-        setUsers(res.results)
-        originalUsers.current = res.results
+    setLoading(true);
+    setError(false);
+
+    fetchUsers(currentPage)
+    .then(users => {
+        // <- Resuelve la promesa
+        setUsers(prevUsers => {
+          const newUsers = prevUsers.concat(users)
+          originalUsers.current = newUsers
+          return newUsers
+        });
       })
-      .catch(err => {
-        console.error(err)
+      .catch((err) => {
+        // <- Pillar los errores
+        setError(err);
+        console.error(err);
       })
-  }, [])
+      .finally(() => {
+        // <- Se ejecuta siempre
+        setLoading(false);
+      });
+  }, [currentPage]);
 
   const filteredUsers = useMemo(() => {
-    console.log('calculate filteredUsers')
+    console.log("calculate filteredUsers");
     return filterCountry != null && filterCountry.length > 0
-      ? users.filter(user => {
-        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
-      })
-      : users
-  }, [users, filterCountry])
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase());
+        })
+      : users;
+  }, [users, filterCountry]);
 
   const sortedUsers = useMemo(() => {
-    console.log('calculate sortedUsers')
+    console.log("calculate sortedUsers");
 
-    if (sorting === SortBy.NONE) return filteredUsers
+    if (sorting === SortBy.NONE) return filteredUsers;
 
     const compareProperties: Record<string, (user: User) => any> = {
-      [SortBy.COUNTRY]: user => user.location.country,
-      [SortBy.NAME]: user => user.name.first,
-      [SortBy.LAST]: user => user.name.last
-    }
+      [SortBy.COUNTRY]: (user) => user.location.country,
+      [SortBy.NAME]: (user) => user.name.first,
+      [SortBy.LAST]: (user) => user.name.last,
+    };
 
     return filteredUsers.toSorted((a, b) => {
-      const extractProperty = compareProperties[sorting]
-      return extractProperty(a).localeCompare(extractProperty(b))
-    })
-  }, [filteredUsers, sorting])
-
-  // const filteredUsers = (() => {
-  //   console.log('calculate filteredUsers')
-  //   return filterCountry != null && filterCountry.length > 0
-  //     ? users.filter(user => {
-  //       return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
-  //     })
-  //     : users
-  // })()
-
-  // const sortedUsers = (() => {
-  //   console.log('calculate sortedUsers')
-
-  //   return sortByCountry
-  //     ? filteredUsers.toSorted(
-  //       (a, b) => a.location.country.localeCompare(b.location.country)
-  //     )
-  //     : filteredUsers
-  // })()
+      const extractProperty = compareProperties[sorting];
+      return extractProperty(a).localeCompare(extractProperty(b));
+    });
+  }, [filteredUsers, sorting]);
 
   return (
     <div className="App">
       <h1>Prueba técnica</h1>
       <header>
-        <button onClick={toggleColors}>
-          Colorear files
-        </button>
+        <button onClick={toggleColors}>Colorear files</button>
 
         <button onClick={toggleSortByCountry}>
-          {sorting === SortBy.COUNTRY ? 'No ordenar por país' : 'Ordenar por país'}
+          {sorting === SortBy.COUNTRY
+            ? "No ordenar por país"
+            : "Ordenar por país"}
         </button>
 
-        <button onClick={handleReset}>
-          Resetear estado
-        </button>
+        <button onClick={handleReset}>Resetear estado</button>
 
-        <input placeholder='Filtra por país' onChange={(e) => {
-          setFilterCountry(e.target.value)
-        }} />
-
+        <input
+          placeholder="Filtra por país"
+          onChange={(e) => {
+            setFilterCountry(e.target.value);
+          }}
+        />
       </header>
       <main>
-        <UsersList changeSorting={handleChangeSort} deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
+        {users.length > 0 && (
+          <UsersList
+            changeSorting={handleChangeSort}
+            deleteUser={handleDelete}
+            showColors={showColors}
+            users={sortedUsers}
+          />
+        )}
+        {loading && <strong>Cargando....</strong>}
+        {error && <strong>Ha ocurrido un error</strong>}
+        {!error && users.length === 0 && (
+          <strong>No hay usuarios</strong>
+        )}
+        {!loading && !error && (
+          <button onClick={() => setCurrentPage(currentPage + 1)}>
+            Cargar más resultados
+          </button>
+        )}
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
