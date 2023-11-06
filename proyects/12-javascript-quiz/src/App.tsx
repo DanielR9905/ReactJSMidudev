33078b231b94e24f7e25d761d59a2a708def38eb
DanielR9905 <danielramirezduque1@gmail.com>
2023-11-06 17:15:10 -0500

    Progress #2 js quiz - configurations rendered info this json

--- proyects/12-javascript-quiz/src/App.tsx
+++ proyects/12-javascript-quiz/src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { Container, Stack, Typography } from "@mui/material";
+import "./App.css";
+import JavaScriptLogo from "./JavaScriptLogo";
+import { Start } from "./Start";
+import { useQuestionStore } from "./store/questions";
+import { Game } from "./Game";
+
 
 function App() {
 
+  const questions = useQuestionStore(state => state.questions)
+  console.log(questions);
+  
   return (
-    <>
-      <h1>hello dr</h1>
-    </>
-  )
+    <main>
+      <Container maxWidth="sm">
+          <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+            <JavaScriptLogo/>
+            <Typography variant="h2" component="h1">
+              JavaScript Quizz
+            </Typography>
+          </Stack>
+
+          {questions.length === 0 && <Start/>}
+          {questions.length > 0 && <Game/>}
+      </Container>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ proyects/12-javascript-quiz/src/Game.tsx
@@ -0,0 +1,43 @@
+// import { IconButton, Stack } from "@mui/material"
+
+import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from '@mui/material'
+import { useQuestionStore } from "./store/questions"
+import SyntaxHighLighter from "react-syntax-highlighter"
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs"
+import { type Question as QuestionType} from "./types"
+
+const Question = ({info} : {info: QuestionType}) => {
+    return (
+        <Card variant="outlined" sx={{bgcolor:'#222', p: 2 ,textAlign: 'left'}}>
+            <Typography variant="h5">
+                {info.question}
+            </Typography>
+
+            <SyntaxHighLighter language="javascript" style={gradientDark}>
+                {info.code}
+            </SyntaxHighLighter>
+
+            <List sx={{bgcolor: '#333', textAlign:'center'}}>
+                {info.answers.map((answer, index) => (
+                    <ListItem key={index} disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary={answer} sx={{ textAlign: 'center'}}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Card>
+    )
+}
+export const Game = () => {
+
+    const questions = useQuestionStore(state => state.questions)
+    const currentQuestion = useQuestionStore(state => state.currentQuestion)
+
+    const questionInfo = questions[currentQuestion]
+    return (
+        <>
+            <Question info={questionInfo}/>
+        </>
+    )
+}
